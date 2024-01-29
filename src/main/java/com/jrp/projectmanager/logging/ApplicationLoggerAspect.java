66cb3ca3a190e962414adbc1656e7319bcdc29bf
nylespham <nylespham@gmail.com>
2024-01-29 22:55:21 +0700

    aspect

---
+++ src/main/java/com/jrp/projectmanager/logging/ApplicationLoggerAspect.java
@@ -0,0 +1,25 @@
+package com.jrp.projectmanager.logging;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ApplicationLoggerAspect {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    @Pointcut("within(com.jrp.projectmanager.controllers..*)")
+    public void definePackagePointcuts(){
+        // empty method just to name the location specified in the pointcut
+
+
+    }
+    @Before("definePackagePointcuts()")
+    public void log(){
+        log.debug(" ----------------------------- ");
+    }
+}
